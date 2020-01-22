6cee131db5e7ee6d2f5828493b72c5e4fe433b98
GForrow <55759891+GForrow@users.noreply.github.com>
2020-01-22 20:59:35 +0000

    Add files via upload
    Uploading two examples of scripts creating in my time at DataVita.

---
+++ Backups vs VMs.ps1
@@ -0,0 +1,51 @@
+﻿
+##*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##
+## Script Quick Guide                                                                                                    ##
+## -- Open this script within "Windows Powershell ISE(x86)" to execute.                                                  ##
+## -- This script is to be run on a local machine only. DO NOT RUN ON SERVERS.                                           ##
+## -- Drop this script in an empty folder with exported data from both VSphere and Rubrik. (must be .csv)                ##
+## -- VSphere report should only need generated ~every 2 weeks or when a new client backup configuration is complete     ##
+## -- Rubrik data can be taken from the "Daily Detail Report" sent to the service desk mailbox each day.                 ##
+## -- Name each data set "VSphereData.csv" and "RubrikData.csv" accordingly.                                             ##
+## -- Highlight whole script and press F8 to run. Console will detail any errors.                                        ##
+##*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##
+
+##Get Current Script Directory. Works in both ISE and executed script. 
+function Get-ScriptDirectory {if ($psise) {Split-Path $psise.CurrentFile.FullPath}
+    else {$global:PSScriptRoot}}
+
+##Set scripts home location found in above function. 
+$scriptHome = Get-ScriptDirectory
+
+##Generate a path to export the report next to the script.
+$exportPath = $scriptHome + "\ComparisonResult.csv"
+$confirmedResult = $scriptHome + "\ConfirmedBackups.csv"
+$failedResult = $scriptHome + "\NoBackups.csv"
+
+##Importing Data Sets - Only needing the column listing Virtual Machines
+$vSphereData = Import-CSV -Path $scriptHome"\VSphereData.csv" | Where-Object {($_.'State' -eq "Powered On") } | Select-Object 'Name'
+
+##Importing RubrikData from "Daily Detail Report" received by ServiceDesk. Where we need only the objects with attributes "VSphere VM" and "Backup".
+$rubrikData = Import-CSV -Path $scriptHome"\RubrikData.csv" | Where-Object {($_.'Object Type' -eq "vSphere VM") } | Select-Object 'Object Name', 'SLA Domain'  ##-and ($_.'Task Type' -like "Backup")
+
+##Creating arrays for each list of virtual machines from VSphere and Rubrik data sets. 
+$rVM = $rubrikData.'Object Name'
+$vsVM = $vSphereData.Name
+
+##We compare vSphere objects to Rubrik list and make sure all vSphere objects are present in Rubrik. 
+##For each VM listed in VSphere data, make sure it is included in Rubrik data. If not, add to exported CSV. 
+foreach ($VirtualMachine in $vsVM){
+    if ($rVM -notcontains $VirtualMachine)
+    {
+       Write-Host "This machine is missing from Rubrik: " $VirtualMachine ##Output to confirm a difference is found.
+       $exportObject = $VirtualMachine | Select-Object @{Name="VM's Missing from Rubrik";Expression={$_}} ##Create export object to handle and present data correctly
+       $exportObject | Export-Csv -Path $failedResult -NoTypeInformation -append -Force ##Add the resulting VM to "Comparison Results.csv" 
+    }
+    else
+    {
+       Write-Host $VirtualMachine " is being backed up in Rubrik." 
+       $exportObject = $VirtualMachine | Select-Object @{Name="VM's backed up in Rubrik";Expression={$_}}, @{Name="SLA";Expression={ $_}}  ##Create export object to handle and present data correctly
+       $exportObject | Export-Csv -Path $confirmedResult -NoTypeInformation -append -Force ##Add the resulting VM to "Comparison Results.csv" 
+    }
+}
+
---
+++ Comparing Spreadsheets.ps1
@@ -0,0 +1,94 @@
+﻿##*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##
+## Script Quick Guide                                                                                                    ##
+## -- Open this script within "Windows Powershell ISE to execute.                                                        ##
+## -- This script is to be run on a local machine only. DO NOT RUN ON SERVERS.                                           ##
+##                                                                                                                       ##
+## Script Notes Still To Be Written Script Notes Still To Be Written Script Notes Still To Be Written Script Notes Still ##
+##*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##
+##DV-All Customer Cards = Gallagher
+##customer_contact = ServiceNow
+
+###### Importing Data ######
+
+##Get Current Script Directory. Works in both ISE and executed script. 
+function Get-ScriptDirectory {if ($psise) {Split-Path $psise.CurrentFile.FullPath}
+    else {$global:PSScriptRoot}}
+
+##Set scripts home location found in above function. 
+$scriptHome = Get-ScriptDirectory
+
+##Generate a path to export the report next to the script.
+$exportPath = $scriptHome + "\Result\ComparisonResult.csv"
+$exportGal = $scriptHome + "\Result\GalComp.csv"
+$exportSN = $scriptHome + "\Result\SNComp.csv"
+
+
+$resultMatch = $scriptHome + "\Result\inBoth.csv"
+$resultGal = $scriptHome + "\Result\MissingFromSN.csv"
+$resultSN = $scriptHome + "\Result\MissingFromGal.csv"
+
+##Importing Gallagher Data - Selecting First Name, Last Name, and 'Job Title' which holds users card number. 
+$_GData = Import-CSV -Path $scriptHome"\DV-All Customer Cards.csv" | Select-Object 'First Name', 'Last Name', 'Job Title' 
+
+##Importing Users from ServiceNow Data - Selecting Name and Pass Number
+$_SNData = Import-CSV -Path $scriptHome"\customer_contact.csv" | Select-Object 'name', 'Data Centre Pass No'
+
+###### Data Maintenance ######
+#
+##Create a new FullName field for easier comparison between data sets
+$_GData | Add-Member -Membertype noteproperty -Name Name -Value 0
+$_GData | Add-Member -MemberType NoteProperty -Name CardNo -Value 0
+#
+##Do the same for ServiceNow Data
+$_SNData | Add-Member -MemberType NoteProperty -Name CardNo -Value 0
+#
+##############################
+
+##Cleaning up Gallagher data
+foreach ($_Person in $_GData){
+    ##Filling in Full Name field.
+    $_Person.Name = $_Person.'First Name' + " " + $_Person.'Last Name'
+    ##Removing blank spaces from Card Numbers. 
+    $_Person.'Job Title' = $_Person.'Job Title' -replace '\s','' 
+    $_Person.CardNo = $_Person.'Job Title'
+}
+
+foreach ($_Card in $_SNData)
+{
+    $_Card.CardNo = $_Card.'Data Centre Pass No';
+}
+
+##Tidy up Usernames in Service Now data
+foreach ($_User in $_SNData){
+    $_User.name = $_User.name -replace '  ',' '
+}
+
+$_GComp = $_GData | Select-Object Name, CardNo   
+$_SNComp = $_SNData | Select-Object Name, CardNo 
+
+$_GComp | Export-Csv -Path $exportGal -NoTypeInformation -append -Force
+$_SNComp| Export-Csv -Path $exportSN -NoTypeInformation -append -Force
+
+Compare-Object $_GComp $_SNComp -Property Name, cardno -IncludeEqual | Export-Csv -Path $exportPath -NoTypeInformation -append -Force
+
+$_CompResult = Import-CSV -Path $scriptHome"\Result\ComparisonResult.csv" | Select-Object name, cardno, sideindicator
+
+
+foreach ($_result in $_CompResult)
+{ 
+    if("<=" -in $_result.SideIndicator){
+       Write-Host "Only Gallagher: " $_result.name " " $_result.cardno ##Output to confirm a difference is found.
+       $exportObject = $_result | Select-Object @{Name="User:";Expression={$_.name}},@{Name="Card Number: ";Expression={$_.cardno}},@{Name="Gallagher";Expression={'X'}},@{Name="ServiceNow";Expression={''}} ##Create export object to handle and present data correctly
+       $exportObject | Export-Csv -Path $resultGal -NoTypeInformation -append -Force ##Add the resulting VM to "Comparison Results.csv" 
+    }
+    elseif("=>" -in $_result.SideIndicator){
+       Write-Host "Only ServiceNow: " $_result.name " " $_result.cardno ##Output to confirm a difference is found.
+       $exportObject = $_result | Select-Object @{Name="User:";Expression={$_.name}},@{Name="Card Number: ";Expression={$_.cardno}},@{Name="Gallagher";Expression={''}},@{Name="ServiceNow";Expression={'X'}} ##Create export object to handle and present data correctly
+       $exportObject | Export-Csv -Path $resultGal -NoTypeInformation -append -Force ##Add the resulting VM to "Comparison Results.csv" 
+    }
+    elseif("==" -in $_result.SideIndicator){
+       Write-Host "Present in both: " $_result.name " " $_result.cardno ##Output to confirm a difference is found.
+       $exportObject = $_result | Select-Object @{Name="User:";Expression={$_.name}},@{Name="Card Number: ";Expression={$_.cardno}},@{Name="Gallagher";Expression={'X'}},@{Name="ServiceNow";Expression={'X'}} ##Create export object to handle and present data correctly
+       $exportObject | Export-Csv -Path $resultGal -NoTypeInformation -append -Force ##Add the resulting VM to "Comparison Results.csv" 
+    }
+}
